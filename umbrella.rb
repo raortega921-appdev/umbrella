09e37c6bf079b13fb8ba0225a75a6df1acc51532
Rafael Ortega <rortega@chicagobooth.edu>
2023-04-06 16:23:54 +0000

    umbrella almost done

--- umbrella.rb
+++ umbrella.rb
@@ -34,5 +34,46 @@ p longitude
 pirate_weather_url = "https://api.pirateweather.net/forecast/#{ENV.fetch("PIRATE_WEATHER_KEY")}/#{latitude},#{longitude}"
 
 pirate_raw_response = URI.open(pirate_weather_url).read
+parsed_pirate_weather_data = JSON.parse(pirate_raw_response)
 
-parsed_pirate_weather_data = JSON.parse
+current_weather = parsed_pirate_weather_data.fetch("currently")
+temp = current_weather.fetch("temperature")
+humidity = current_weather.fetch("humidity")
+summary = current_weather.fetch("summary")
+rain = current_weather.fetch("precipProbability")
+
+p "It is currently #{summary} and #{temp} degress outside with a humidity of #{humidity}. There is a #{rain} percent chance of rain."
+
+minutely_hash = parsed_pirate_weather_data.fetch("minutely", false)
+if minutely_hash
+  next_hour_summary = minutely_hash.fetch("summary")
+
+  puts "Next hour: #{next_hour_summary}"
+end
+
+hourly_hash = parsed_pirate_weather_data.fetch("hourly", false)
+if hourly_hash
+  day_hash = hourly_hash.fetch("summary", false)
+
+  puts "Next 12 hours: #{day_hash}"
+end
+
+
+
+hourly_rain = parsed_pirate_weather_data.fetch("hourly")
+hourly_array = hourly_rain.fetch("data")
+
+day_summary = hourly_array[1..12]
+
+precip_acceptance_level = 0.10
+
+threshold = false
+
+day_summary.each do |hourly_hash|
+
+min_rain_probability = hourly_hash.fetch("precipProbability")
+
+if precip_prob > precip_prob_threshold
+  its_gon_rain = true
+
+  
